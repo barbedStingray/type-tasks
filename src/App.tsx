681afd538b2885f8d86475067804a67bf908a2fd
Ben Rehmann <brehmann@me.com>
2024-08-26 12:34:27 -0500

    adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,49 @@ import { Task } from './tasks'
 
 function App() {
 
-  console.log(TaskList)
   const [taskList, setTaskList] = useState<Task[]>(TaskList)
 
+  const [newTask, setNewTask] = useState<string>('')
+
+
+
+  function addNewTask(newTask: string) {
+    console.log('adding new task', newTask)
+
+    // find minimum unused id...
+    const minimumId = taskList.map((task) => task.id).sort((a, b) => a - b).filter((x, i, arr) => arr[i - 1] !== x)
+      .filter((x, i, arr) => arr[i] !== i)
+    const newIndexNumber = minimumId.length !== 0 ? minimumId[0] - 1 : minimumId.length
+    console.log(newIndexNumber)
+
+    const taskObject: Task = {
+      id: newIndexNumber,
+      task: newTask,
+      complete: false,
+    }
+
+    setTaskList(prevTasks => [...prevTasks, taskObject])
+    setNewTask('')
+
+
+
+  }
 
 
 
   return (
     <div className="App">
+
+      <h1>Add a new Task Here</h1>
+      <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+      <button onClick={() => addNewTask(newTask)}>Add Task</button>
+
+      {taskList.map((task, i) => (
+        <div key={i}>
+          <h3 >{task.task}</h3>
+          <button>Complete</button>
+        </div>
+      ))}
 
     </div>
   );
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,26 +1,22 @@
 export interface Task {
+    id: number,
     task: string,
     complete: boolean
 }
 
 const tasks: Task[] = [
     {
-        task: 'Weigh the shrimp',
-        complete: false,
-    },
-    {
-        task: 'Walk the Beach',
-        complete: false,
-    },
-    {
+        id: 0,
         task: 'Swim the Lagoon',
         complete: false,
     },
     {
+        id: 1,
         task: 'Feed the Stingray',
         complete: false,
     },
     {
+        id: 2,
         task: 'Entertain the Guests',
         complete: false,
     }
